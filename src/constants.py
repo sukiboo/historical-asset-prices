7292fb67afebb324da26abe2acb8980f66005b1d
Anton Dereventsov <dereventsov@gmail.com>
2026-01-07 15:45:54 -0500

    update constants

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,12 @@
-# tickers for price retrieval
-STOCK_TICKERS = ["SPY"]  # list of stock tickers to retrieve
-CRYPTO_TICKERS = ["BTC-USD"]  # list of crypto tickers to retrieve
-FOREX_TICKERS = ["EUR-USD"]  # list of forex tickers to retrieve
+# asset type retrieval flags
+RETRIEVE_STOCKS = True
+RETRIEVE_OPTIONS = True
+RETRIEVE_CRYPTO = True
+RETRIEVE_FOREX = True
 
-# date range for price retrieval
-DATE_START = "2024-04-01"  # start date (YYYY-MM-DD)
-DATE_END = "2024-04-02"  # end date (YYYY-MM-DD)
+# date range for file retrieval
+DATE_START = "2025-01-01"  # start date (YYYY-MM-DD)
+DATE_END = "2025-01-03"  # end date (YYYY-MM-DD)
 
 # data retrieval settings
 MIN_DELAY = 1  # minimum delay between retries (seconds)
